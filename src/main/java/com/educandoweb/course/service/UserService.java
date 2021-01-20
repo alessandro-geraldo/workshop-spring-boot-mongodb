976e8d55fa03a838dba52ccdd75e5cfe01918533
alessandro-geraldo <alessandro.com@hotmail.com>
2021-01-20 11:59:02 -0300

    Obtendo um usuário por id

--- src/main/java/com/educandoweb/course/service/UserService.java
+++ src/main/java/com/educandoweb/course/service/UserService.java
@@ -1,12 +1,14 @@
 package com.educandoweb.course.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.domain.User;
 import com.educandoweb.course.repository.UserRepository;
+import com.educandoweb.course.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,6 +20,11 @@ public class UserService {
 		return repo.findAll();
 		
 		
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("objeto nao encontrado"));
 	}
 
 }
