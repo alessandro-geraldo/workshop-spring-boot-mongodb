e4774f6afc2f0186a7afc49c4d0f591808cfea98
alessandro-geraldo <alessandro.com@hotmail.com>
2021-01-20 19:00:05 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/educandoweb/course/repository/PostRepository.java
+++ src/main/java/com/educandoweb/course/repository/PostRepository.java
@@ -2,8 +2,8 @@ package com.educandoweb.course.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.educandoweb.course.domain.User;
+import com.educandoweb.course.domain.Post;
 
-public interface UserRepository extends MongoRepository<User, String>{
+public interface PostRepository extends MongoRepository<Post, String>{
 
 }
