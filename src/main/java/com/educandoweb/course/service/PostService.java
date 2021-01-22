33820c95efdbea6bf77179c35d287474fe8be3d3
alessandro-geraldo <alessandro.com@hotmail.com>
2021-01-22 11:29:21 -0300

    Obtendo um post por id

---
+++ src/main/java/com/educandoweb/course/service/PostService.java
@@ -0,0 +1,22 @@
+package com.educandoweb.course.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.domain.Post;
+import com.educandoweb.course.repository.PostRepository;
+import com.educandoweb.course.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("objeto nao encontrado"));
+	}
+}
